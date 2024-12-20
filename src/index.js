17531d2cbf6e14d2377dbf2090a51531dfd03402
Matthew West <128643572+MatthewWestDev@users.noreply.github.com>
2024-12-20 17:27:44 -0600

    Completes menu JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles.css";
+
+const flyoutItems = document.querySelectorAll("li.has-submenu");
+
+for (const flyoutItem of flyoutItems) {
+  let timer;
+  flyoutItem.addEventListener("mouseover", function () {
+    this.className = "has-submenu open";
+    this.querySelector("a").setAttribute("aria-expanded", "true");
+    clearTimeout(timer);
+  });
+  flyoutItem.addEventListener("mouseout", function () {
+    timer = setTimeout(function () {
+      const opened = document.querySelector(".has-submenu.open");
+      opened.className = "has-submenu";
+      opened.querySelector("a").setAttribute("aria-expanded", "false");
+    }, 800);
+  });
+}
